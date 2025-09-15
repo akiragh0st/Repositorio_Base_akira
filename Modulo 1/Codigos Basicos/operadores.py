91dbc8a87d68e20d06eadd8bc145c275f3ce4cc8
Isaac de oliveira sales <isaacsalesoliver@gmail.com>
2025-09-15 11:02:29 -0300

    Create operadores.py

---
+++ Modulo 1/Codigos Basicos/operadores.py
@@ -0,0 +1,18 @@
+# Aritméticos 
+Adição = 2 + 2
+print(Adição)
+
+Subtração = 2 - 2
+print(Adição)
+
+Multiplicação = 2 * 2
+print(Multiplicação)
+
+Divisão = 2 / 2
+print(Divisão)
+
+Potencia = 2 ** 2
+print(Potencia)
+
+Divisão_inteira = 22 // 44
+print(Divisão_inteira)
