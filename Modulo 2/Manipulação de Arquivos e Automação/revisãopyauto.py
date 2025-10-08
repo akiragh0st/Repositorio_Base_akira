4fd32ebb24962f596b7441247e170cc2c765ab9c
Akira_gh0st <isaacsalesoliver@gmail.com>
2025-10-08 08:31:43 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/revisãopyauto.py
@@ -0,0 +1,29 @@
+import pyautogui as gui
+import time
+
+gui.press("win")
+gui.sleep(0.4)
+gui.write("brave")
+gui.sleep(0.4)
+gui.press("Enter")
+gui.sleep(1)
+gui.click(1895,62)
+gui.sleep(0.4)
+gui.click(1599,469)
+gui.sleep(0.4)
+gui.click(1450,500)
+gui.sleep(0.4)
+gui.click(926,167)
+gui.sleep(0.4)
+gui.write("bonjourr")
+gui.sleep(0.2)
+gui.press("enter")
+gui.sleep(3)
+gui.click(833,267)
+gui.sleep(3)
+gui.click(1407,229)
+gui.sleep(2)
+gui.click(1007,266)
+gui.sleep(1)
+gui.hotkey("ctrl", "h")#combina tecla
+
